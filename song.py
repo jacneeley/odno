cebca58111da03b154fb36d8d1b8fa31b6a8da58
jacneeley <jacneeley@outlook.com>
2026-02-26 19:16:17 -0600

    updated song to hold album url

--- song.py
+++ song.py
@@ -1,7 +1,7 @@
 import datetime
 
 class Song:
-    def __init__(self, title:str, artist:str, album_artist:str, album:str, cd:int, year:datetime, track_num:int):
+    def __init__(self, title:str, artist:str, album_artist:str, album:str, cd:int, year:datetime, track_num:int, cover:str):
         self.title = title
         self.artist = artist
         self.album_artist = album_artist
@@ -9,9 +9,10 @@ class Song:
         self.cd = cd
         self.year = year
         self.track_num = track_num
+        self.cover = cover
 
     def __str__(self):
-        return f"{self.title} : [artist={self.artist}, album={self.album}, cd={self.cd}, year={self.year}, track_num={self.track_num}]"
+        return f"{self.title} : [artist={self.artist}, album={self.album}, cd={self.cd}, year={self.year}, track_num={self.track_num}, album_cover={self.cover}]"
     
     def __repr__(self):
         return self.__str__()
