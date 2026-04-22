8adac1cacc16fd00387e3ac411c4aec2b566944d
jacneeley <jacneeley@outlook.com>
2026-04-22 10:52:48 -0500

    replaced magic strings with constants, added builders to classes, added responsebody class for managing response, and other misc changes

--- src/fetcher.py
+++ src/fetcher.py
@@ -1,13 +1,17 @@
-import requests
+'''Module for making requests to APIs.'''
 import sys
-
 from bs4 import BeautifulSoup
 
 import discogs_client
-from scripts.globals import load_env, DISCOGS_URL, DISCOGS_USER_TOKEN, LASTFM_KEY, LASTFM_URL, MUSIC_BRAINZ_URL
-from src.utility import convert_date_str
+import scripts.globalconstants as globalconstants
+import scripts.odnologging as odnologging
 
-if not load_env():
+from src.utility import convert_date_str
+from src.models import ResponseBodyBuilder, ResponseBody
+
+logger = odnologging.create_logger("fetcher.py")
+
+if not globalconstants.__loadenv__():
     print("failed to load env vars...")
     sys.exit()
 
@@ -16,10 +20,10 @@ def auth_user() -> discogs_client.Client:
     '''
         Return Client object from discogs using a user_token
     '''
-    d = discogs_client.Client("cdripper/0.1", user_token=DISCOGS_USER_TOKEN())
+    d = discogs_client.Client("cdripper/0.1", user_token=globalconstants.__dicogsusertoken__())
     return d
 
-def get_album_discogs(query:str) -> dict:
+def get_album_discogs(query:str) -> ResponseBody:
     '''
         Use discog's authorized client to search its api for an album via its "master_id".
 
@@ -31,11 +35,16 @@ def get_album_discogs(query:str) -> dict:
     '''
 
     master_id = auth_user().search(query=query, type="release")[0].data['master_id']
+    url = globalconstants.__discogsurl__() + str(master_id)
 
-    resp:requests.Response = requests.get(DISCOGS_URL() + str(master_id), timeout=20)
-    return resp.json()
+    response_body:ResponseBody = (ResponseBodyBuilder()
+                                  .url(url)
+                                  .debug(globalconstants.__debugflg__())
+                                  .build())
+    
+    return response_body.get()
 
-def get_album_lastfm(artist:str, album:str, debug:bool) -> dict:
+def get_album_lastfm(artist:str, album:str) -> ResponseBody:
     '''
         Search lastfm for album data using album name and artist name.
 
@@ -47,28 +56,26 @@ def get_album_lastfm(artist:str, album:str, debug:bool) -> dict:
         returns:
             * JSON response.
     '''
-    api_key = f"&api_key={LASTFM_KEY()}"
+    api_key = f"&api_key={globalconstants.__lastfmkey__()}"
     artist_str = f"&artist={artist}"
     album_str = f"&album={album}"
     format_str = "&format=json"
-    query = LASTFM_URL() + api_key + artist_str + album_str + format_str
+    query = globalconstants.__lasftfmurl__() + api_key + artist_str + album_str + format_str
 
-    if debug:
-        print("searching using:",query)
-
-    resp:requests.Response = requests.get(query, timeout=20)
+    response_body:ResponseBody = (ResponseBodyBuilder()
+                                  .url(query)
+                                  .debug(globalconstants.__debugflg__())
+                                  .build())
     
-    if debug:
-        print("lastfm response:\n", resp.content)
+    if response_body.debug:
+        logger.info("searching using: %s", query, exc_info=1)
 
-    if resp.ok:
-        return resp.json()
+    # if response_body.debug and response_body.response:
+    #     print("lastfm response:\n", response_body.response)
     
-    print("could not get find:",query,"\nresponse status:",resp.status_code)
-    print(resp.raise_for_status())
-    return {}
+    return response_body.get()
 
-def fetch_date_from_music_brainz(mbid:str , debug:bool) -> str:
+def fetch_date_from_music_brainz(mbid:str) -> str:
     '''
         You would think lastfm would return the release date in their api response but they don't.
         To get around that, this method will grab the mbid and use it to search for the album release date on MusicBrainz.
@@ -82,20 +89,32 @@ def fetch_date_from_music_brainz(mbid:str , debug:bool) -> str:
     '''
 
 
-    query = MUSIC_BRAINZ_URL().replace("MBID", mbid)
+    query = globalconstants.__musicbrainzurl__().replace("MBID", mbid)
     
-    resp = requests.get(query, timeout=20)
+    # resp = requests.get(query, timeout=20)
+    response_body = (ResponseBodyBuilder()
+                     .url(query)
+                     .debug(globalconstants.__debugflg__())
+                     .build())
 
-    if debug:
-        print("searching for:",query)
-        print("response:\n",resp)
+    response_body.get()
 
-    if resp.ok:
-        soup = BeautifulSoup(resp.text, features="xml")
-        return convert_date_str(soup.find("first-release-date").string)
+    if response_body.debug:
+        logger.info("searching for: %s",query)
+        logger.info("response:\n%s",response_body.response.text)
+
+    if not response_body.is_success:
+        logger.info("Could not get datetime from musicbrainz...\nresponse code: %s", response_body.response_code)
+        return "N/A"
     
-    print("Could not get datetime from musicbrainz...\n", "response code:", resp.status_code)
-    return "N/A"
+    soup = BeautifulSoup(response_body.response.text, features="xml")
+    target = soup.find("first-release-date").string
+    if not target:
+        logger.info("Could not get datetime from musicbrainz...")
+        return "N/A"
+
+    return convert_date_str(soup.find("first-release-date").string)
+
 
 if __name__ == "__main__":
     print("testing connection with supplied client_id & client_secret...")
@@ -104,7 +123,7 @@ if __name__ == "__main__":
     print(results)
 
     print(get_album_discogs("daydream nation by sonic youth"))
-    print(get_album_lastfm("sonic+youth", "goo", True))
+    print(get_album_lastfm("sonic+youth", "goo"))
 
-    test = fetch_date_from_music_brainz("18eb7b48-83a5-49d8-b8a0-04ee2b123b2d", True)
+    test = fetch_date_from_music_brainz("18eb7b48-83a5-49d8-b8a0-04ee2b123b2d")
     print(test)
--- src/handle_metadata.py
+++ src/handle_metadata.py
@@ -7,10 +7,11 @@ from collections import deque
 import src.fetcher as fetcher
 import src.utility as util
 import src.prompts as prompts
+from src.models import SongBuilder, Song, ResponseBody
+import scripts.globalconstants as globalconstants
+import scripts.odnologging as odnologging
 
-from src.models import Song
-
-TMP_DIR = "MP3_ALBUM"
+logger = odnologging.create_logger("handle_metadata.py")
 
 def check_album(album:dict, platform:str) -> dict:
     '''
@@ -22,168 +23,216 @@ def check_album(album:dict, platform:str) -> dict:
     tmp = {
         "album" : "",
         "artist": "",
-        "release_date": ""
+        "release_date": "",
+        "source": ""
     }
 
-    if platform == "LASTFM":
+    if platform == globalconstants.__lastfm__():
         mbid = album['album']['mbid']
         tmp["album"] = album['album']['name']
         tmp['artist'] = album['album']['artist']
-        tmp["release_date"] =  fetcher.fetch_date_from_music_brainz(mbid, False)
+        tmp['release_date'] =  fetcher.fetch_date_from_music_brainz(mbid)
+        tmp['source'] = globalconstants.__lastfm__()
     
-    elif platform == "DISCOGS":
-        tmp["album"] = album['title']
+    elif platform == globalconstants.__discogs__():
+        tmp['album'] = album['title']
         tmp['artist'] = album['artists'][0]['name']
-        tmp["release_date"] = album['year']
+        tmp['release_date'] = util.convert_date_str(album['year'])
+        tmp['source'] = globalconstants.__discogs__()
     
-    print(
-        "album:",   tmp['album']           +"\n",
-        "artist:",  tmp['artist']          +"\n",
-        "release:", str(tmp['release_date'])    +"\n"
-    )
+    print("album:",    tmp['album'])
+    print("artist:",   tmp['artist'])
+    print("release:",  str(tmp['release_date']))
 
     return tmp
 
-def get_album_data_from_discogs(album:dict, resp:dict, debug:bool) -> list[Song]:
+def get_album_data_from_source(album:dict, resp:dict) -> list[Song]:
     '''
-        retrieve album data from discogs database.
+        retrieve album data from source.
     '''
-    
+
     tracks = []
-
-    album_cover = resp['images'][0]['uri']
-    genre = resp['styles'][0] if len(resp['styles']) > 1 else resp['genres'][0]
-
-    l = len(resp['tracklist'])
-    for t in range(l):
-        song = Song(
-            title=resp["tracklist"][t]['title'],
-            artist=album['artist'],
-            album_artist=album['artist'],
-            album=album['album'],
-            cd = 1,
-            genre=genre,
-            year=resp['year'],
-            track_num= t + 1,
-            cover = album_cover
-        )
-
-        tracks.append(song)
-
-    if debug:
-        print("response:", resp)
-    
-    return tracks
-
-def get_album_data_from_lastfm(album:dict, resp:dict, debug:bool) -> list[Song]:
-    '''
-        retrieve album data from lastfm database.
-    '''
-    
-    tracks = []
-    
+ 
     try:
-        if debug:
-            print(resp)
-            print(album)
+        if globalconstants.__debugflg__():
+            logger.info(resp)
+            logger.info(album)
 
-        album_cover = "" 
+        album_cover = ""
         genre = ""
-        if len(resp['album']['image']) > 0:
-            album_cover = resp['album']['image'][3]["#text"]
+        release = "1970-01-01"
+        source = album['source']
+        track_str = ""
 
-        if resp['album']["tags"] != "":
-            genre = resp['album']["tags"]["tag"][0]["name"]
+        json_resp = {}
 
-        release = album['release_date']
+        if source == globalconstants.__discogs__():
+            album_cover = resp['images'][0]['uri']
+            genre = resp['styles'][0] if len(resp['styles']) > 1 else resp['genres'][0]
+            release = album['release_date']
+            track_str = 'title'
+            json_resp = resp['tracklist']
+        
+        else:
+            if len(resp['album']['image']) > 0:
+                album_cover = resp['album']['image'][3]["#text"]
+
+            if resp['album']["tags"] != "":
+                genre = resp['album']["tags"]["tag"][0]["name"]
+
+            release = album['release_date']
+            track_str = 'name'
+            json_resp = resp['album']['tracks']['track']
 
         track_num = 1
-        for t in resp["album"]["tracks"]["track"]:
-            song = Song(
-                title = t['name'],
-                artist = album['artist'],
-                album_artist= album['artist'],
-                album = album['album'],
-                cd=1,
-                genre=genre,
-                year= release,
-                track_num = track_num,
-                cover = album_cover
-            )
+        for t in json_resp:
+            song = (SongBuilder()
+            .title(t[track_str])
+            .artist(album['artist'])
+            .album(album['album'])
+            .album_artist(album['artist'])
+            .genre(genre)
+            .year(release)
+            .track_num(track_num)
+            .cover(album_cover)
+            .build())
+
             track_num += 1
             tracks.append(song)
 
-        if debug:
-            print()
-            print(tracks)
+        if globalconstants.__debugflg__():
+            logger.info(tracks)
 
     except KeyError as ke:
-        print("album data could not be retrieved...")
-        if debug:
-            print("KeyError in JSON response:",ke)
-    
+        logger.error("album data could not be retrieved...")
+        if globalconstants.__debugflg__():
+            logger.error("KeyError in JSON response: %s", ke)
+
+    except ValueError as ve:
+        logger.error("album data could not be retrieved...")
+        if globalconstants.__debugflg__():
+            logger.error("ValueError ocurred building Song: %s", ve)
+
     return tracks
+
+def valid_discogs_flow(album_query:str) -> list[Song]:
+    '''Application will check discogs if lastfm search fails or returns incorrect results.'''
+    album_query = album_query.replace("-", " by ").replace("_", " ")
+    resp = fetcher.get_album_discogs(album_query)
+
+    if not resp.is_success:
+        logger.error("Album data could not be found...")
+        if resp.exception and resp.debug:
+            for e in resp.exception:
+                logger.exception(e)
+        return []
+
+    album = check_album(resp.response_json, globalconstants.__discogs__())
+
+    if input("is the above correct (y/n)?").lower() == globalconstants.__yes__():
+        return get_album_data_from_source(album, resp.response_json)
+
+    return []
+
+def retry_switch(choice, album_query) -> list[Song]:
+    '''Switch statement for retries. Refer to retry().'''
+    if choice == globalconstants.__retry__():
+        return valid_discogs_flow(album_query)
+
+    if choice == globalconstants.__manualsearch__():
+        return manual_search()
+
+    if choice == globalconstants.__manualentry__():
+        return [] ##TODO: create manual entry
+
+    return []
+
+def retry(album_query:str, discogs_failed:bool) -> list[Song]:
+    '''Fallback if a search fails.'''
+    try:
+        choices = deque([globalconstants.__retry__(), globalconstants.__manualsearch__(), globalconstants.__manualentry__()])
+
+        if discogs_failed:
+            choices.popleft()
+
+        prompts.retry_choice_prompt(discogs_failed)
+
+        sel = int("selection: ")
+        return retry_switch(choices[sel], album_query)
+
+    except IndexError:
+        logger.error("Invalid selection was made...Try again.")
+        retry(album_query, discogs_failed)
 
 def manual_search() -> list[Song]:
     '''
         Prompt user to perform a manual search if an automated one can't be done or returns undesirable results.
     '''
     q = input("Would you like to do a manual search (y/n)? ")
-    if q.lower() == "y":
+    if q.lower() == globalconstants.__yes__():
         artist_name = input("enter artist name: ")
         album_name = input("enter album name: ")
 
         print(f"\n searching for {album_name} by {artist_name}")
-        resp = fetcher.get_album_lastfm(artist_name, album_name, True)
+        resp = fetcher.get_album_lastfm(artist_name, album_name)
 
-        album = check_album(resp, "LASTFM")
+        album = check_album(resp.response_json, globalconstants.__lastfm__())
 
         q = input("is the above correct (y/n)? ")
-        if q.lower() == "y":
-            return get_album_data_from_lastfm(album, resp, True)
+        if q.lower() == globalconstants.__yes__():
+            return get_album_data_from_source(album, resp.response_json)
 
         prompts.wow_niche()
         return []
 
     return []
 
-def get_album_from_repo(album_query:str, debug:bool) -> list[Song]:
+def get_response_from_repo(album_query:str) -> ResponseBody:
     '''
         program api to query web services for album metadata.
         
         parameters:
             *album_query -> album query string
-            *debug -> turn debug on
     '''
 
-    resp: dict = {}
+    resp = ResponseBody()
 
     query = album_query.split("-")
     artist_str = query[-1].replace(" ", "+").replace("_","+")
     album_str = query[0].replace(" ", "+").replace("_", "+")
-    
-    resp = fetcher.get_album_lastfm(artist_str, album_str, debug)
-    
-    print(f"\nsearching for {album_query}.")
-    album = check_album(resp, "LASTFM")
-    
-    q:str = input("is the above correct (y/n)? ")
-    if q.lower() == "y":
-        print("getting album metadata...\n")
-        return get_album_data_from_lastfm(album, resp, debug)
 
-    if q.lower() == "n":
-        q = input("try again (y/n)? ")
-        if q.lower() == "y":
-            album_query = album_query.replace("-", " by ").replace("_", " ")
-            resp = fetcher.get_album_discogs(album_query)
-            album = check_album(resp, "DISCOGS")
-            
-            q = input("is the above correct (y/n)?")
-            if q.lower() == "y":
-                return get_album_data_from_discogs(album, resp, debug)
-    
-    return []
+    resp = fetcher.get_album_lastfm(artist_str, album_str)
+
+    if not resp.is_success:
+        logger.warning("Album could not be found.")
+        result_list = retry(album_query, False)
+        if result_list:
+            resp.result_list = result_list
+            resp.is_success = True
+        elif globalconstants.__debugflg__():
+            logger.error("Retry failed...")
+
+    else:
+        print("searching for %s", album_query)
+        album = check_album(resp.response_json, globalconstants.__lastfm__())
+
+        q:str = input("is the above correct (y/n)? ")
+        if q.lower() == globalconstants.__yes__():
+            print("getting album metadata...\n")
+            result_list = get_album_data_from_source(album, resp.response_json)
+            if result_list:
+                resp.result_list = result_list
+            elif globalconstants.__debugflg__():
+                logger.error("album data could not be retrieved from source...")
+
+        if q.lower() == globalconstants.__no__() and input("try again (y/n)? ").lower() == globalconstants.__yes__():
+            result_list = valid_discogs_flow(album_query)
+            if result_list:
+                resp.result_list = result_list
+            elif globalconstants.__debugflg__():
+                logger.error("discogs list came back empty...")
+
+    return resp
 
 def get_album_image(path:str, album_url:str) -> bool:
     '''
@@ -205,36 +254,39 @@ def get_album_image(path:str, album_url:str) -> bool:
         with open(os.path.join(path,"cover.jpg"), "wb") as image:
             data = requests.get(album_url, stream=True, timeout=20)
             if not data.ok:
-                print(f'failed to get image from server - error: {data.status_code}')
+                logger.warning('failed to get image from server - %s', data.status_code)
                 return False
-            
+
             for img in data.iter_content(1024):
                 if not img:
-                    print('failed to get image from server - error: data chunk came back None')
+                    logger.warning('failed to get image from server - data chunk came back None')
                     return False
-                
-                print(f'downloading image from {album_url}...')
+
+                print('downloading image from %s...', album_url)
                 image.write(img)
-            
+
         return True
+
     except OSError as e:
-        print("failed to save image.")
-        print(e)
+        logger.error("failed to save image.")
+        if globalconstants.__debugflg__():
+            logger.exception(e)
+
         return False
 
 
-def modify_metadata_ffmpeg(path:str, file:str, song:Song, bit_rate:int, is_saved:bool, debug) -> bool:
+def modify_metadata_ffmpeg(path:str, file:str, song:Song, bit_rate:int, is_saved:bool) -> bool:
     '''
         Format a series of ffmpeg commands to add metadata to audio files.
         Yup, this bad boy is an ffmpeg wrapper.
     '''
-    
+
     # ftype = file.split(".")
     try:
         ftype = file.split(".")[-1]
-        is_mp3 = True if ftype == "mp3" else False
+        is_mp3 = True if ftype == globalconstants.__mp3__() else False
 
-        parent_dir = os.path.join(os.path.dirname(path), TMP_DIR)
+        parent_dir = os.path.join(os.path.dirname(path), globalconstants.__tmpdir__())
 
         source_file = os.path.join(os.path.dirname(parent_dir), file) if " " not in file else os.path.join(os.path.dirname(parent_dir), f"'{file}'")
 
@@ -247,33 +299,41 @@ def modify_metadata_ffmpeg(path:str, file:str, song:Song, bit_rate:int, is_saved
         final_mp3_file = os.path.join(dest_file.split(title)[0] , f'{song.track_num}_{new_mp3_title}')
 
         cp_cmd = prompts.copy_to_temp(source_file, dest_file)
-        print(cp_cmd, "\n") if debug else ""
+        if globalconstants.__debugflg__():
+            logger.info(cp_cmd, "\n")
+        
         subprocess.run([cp_cmd], shell=True, check=False)
 
         if not is_mp3:
             convert = prompts.convert_to_mp3_with_selected_bitrate(source_file, bit_rate, og)
-            print(convert, "\n") if debug else print()
+            if globalconstants.__debugflg__():
+                logger.info(convert, "\n")
+            
             subprocess.run([convert], shell=True, check = False)
 
         ffmpeg_meta_cmd = prompts.save_metadata_ffmpeg(is_saved, og, parent_dir, song, final_mp3_file)
-        print(ffmpeg_meta_cmd, "\n") if debug else print()
+        if globalconstants.__debugflg__():
+            logger.info(ffmpeg_meta_cmd, "\n")
+        
         subprocess.run([ffmpeg_meta_cmd], shell=True, check = False)
 
         rm_cmd = prompts.clean_up(og)
-        print(rm_cmd,"\n") if debug else print()
+        if globalconstants.__debugflg__():
+            logger.info(rm_cmd,"\n")
+        
         subprocess.run([f'rm {og}'], shell=True, check=False)
-    
+
     except Exception as e:
-        print("failed...")
-        
-        if debug:
-            print(e)
-        
+        logger.error("failed to convert files...")
+
+        if globalconstants.__debugflg__():
+            logger.exception(e)
+
         return False
 
     return True
 
-def save_album_metadata(debug:bool) -> bool:
+def save_album_metadata() -> bool:
     '''
         Consume user provided file path to prepare metadata for target album.
 
@@ -284,23 +344,23 @@ def save_album_metadata(debug:bool) -> bool:
     '''
 
     print("For best results, make sure album folders match the following: album_name-artist_name.\n")
-    
+
     path = input("enter album file path: ")
 
     success = False
     if os.path.exists(path) and os.path.isdir(path):
         album_name = path.split("/")[-1]
 
-        print("searching for:",album_name)
-        
-        tmp:str = os.path.join(path, TMP_DIR)
-        
-        if debug:
-            print("tmp_dir:", tmp)
+        # print("searching for: %s",album_name)
+
+        tmp:str = os.path.join(path, globalconstants.__tmpdir__())
+
+        if globalconstants.__debugflg__():
+            logger.info("tmp_dir: %s", tmp)
 
         if os.path.exists(tmp):
             subprocess.call(f"rm -r {tmp}",shell=True)
-        
+
         dir_list = os.listdir(path)
         util.sort_tracks(dir_list)
 
@@ -308,13 +368,22 @@ def save_album_metadata(debug:bool) -> bool:
 
         os.mkdir(tmp)
 
-        if debug:
-            print("\ntrack list:",dir_list)
+        if globalconstants.__debugflg__():
+            logger.info("\ntrack list: %s",dir_list)
 
-        tracks = get_album_from_repo(album_name, debug)
+        odno_response = get_response_from_repo(album_name)
+
+        if not odno_response.is_success:
+            if globalconstants.__debugflg__():
+                odno_response.show_exceptions()
+            print("Could not get album data...")
+            return False
+
+        tracks = odno_response.result_list
         if len(tracks) == 0:
             tracks = manual_search()
             if len(tracks) == 0:
+                ##TODO: manual_entry()
                 return False
 
         bit_rate = prompts.get_bit_rate()
@@ -324,25 +393,16 @@ def save_album_metadata(debug:bool) -> bool:
             if not is_saved:
                 is_saved = get_album_image(tmp, i.cover)
             dir_track = dir_list.popleft()
-            success = modify_metadata_ffmpeg(tmp, dir_track, i, bit_rate, is_saved, debug)
+            success = modify_metadata_ffmpeg(tmp, dir_track, i, bit_rate, is_saved)
 
             if not success:
                 print("conversion failed...")
                 break
-    
+
     if success:
         util.remove_wavs(tmp)
 
     return success
 
 if __name__ == "__main__":
-    # query:str = "daydream nation by sonic youth"
-
-    # tracks:list[Song] = get_album_from_repo(query, True)
-    # # print(tracks)
-
-    # print("\n\ntracks:")
-    # for t in tracks:
-    #     print(t)
-
-    save_album_metadata(True)
+    save_album_metadata()
