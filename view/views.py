8dea32f07bf7b7fbb7b9f140043b033065b27037
jacneeley <jacneeley@outlook.com>
2026-08-25 09:41:08 -0500

    Revert "#8"
    This reverts commit 4d12b1fa1ea2a8e166dead22584470786c4ec946.

--- view/views.py
+++ view/views.py
@@ -1,9 +1,8 @@
 '''views'''
 from core.rip import rip
-from view.prompts import metadata_menu, main_menu, show_help, show_settings
-from exceptions.odno_exceptions import OdnoException
+from core.prompts import metadata_menu, main_menu
+from core.odnoexceptions import OdnoException
 from src.handle_metadata import do_process
-from core.odno_settings import sel_setting
 
 from core.utility import clean_input_int
 
@@ -13,20 +12,13 @@ def ui_driver() -> bool:
         main_menu()
         sel = clean_input_int("Menu Selection: ")
         if sel == 1:
-            show_help()
-            return ui_driver()
-        elif sel == 2:
             try:
                 rip()
             except OdnoException as oe:
                 OdnoException.handle_exception(oe, "Disk path could not be constructed, could not be found, or there was nothing on the disk.\nLikely cause: Disc drive could not be accessed.")
                 return False
-        elif sel == 3:
+        elif sel == 2:
             metadata_selection()
-        elif sel == 4:
-            show_settings()
-            if sel_setting():
-                return ui_driver()
         else:
             print("\ninvalid selection. try again...")
             return ui_driver()
